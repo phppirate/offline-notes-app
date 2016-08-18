747410357830c2deb7d634f176d83219d4bf5b98
Sam Podlogar <sam@podlogar.com>
2016-08-18 18:19:11 -0500

    Initial Commit #2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Offline Notes
                 </div>
 
                 <div class="links">
